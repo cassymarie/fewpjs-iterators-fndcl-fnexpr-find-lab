9a31b012d264dcc0bebc82299135e3d473e337fa
cassymarie <cassy2205@gmail.com>
2021-02-05 10:22:14 -0700

    commit tree d52af92f6f60d08116db003886740854226110c9

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  let yearWon = array.find(function(win){
+    if (win['result'] === "W"){return win['year'] }})
+
+  if (yearWon){return yearWon['year']}}
